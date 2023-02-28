41e02cf1029ab41275fb1398823a9be9f3bcf04f
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-28 03:31:24 +0100

    validador email da problema; App funciona sobre carga memoria

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
 import { UsersService } from 'src/app/services/users.service';
 
@@ -11,9 +12,12 @@ import { UsersService } from 'src/app/services/users.service';
 export class NewUserComponent {
   miFormulario: FormGroup;
   user: User | any;
+  actualizando = false;
+
 
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute
   ) {
     this.miFormulario = new FormGroup({
       first_name: new FormControl('', [
@@ -37,23 +41,80 @@ export class NewUserComponent {
 
 
   }
-  /* arrUsuarios: Usuario[] = []; */
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      if (params.id) {
+        this.actualizando = true; // Marcar que se está actualizando un usuario existente
+        console.log(params.id)
+        this.usersService.getById(params.id).subscribe(
+          (user: User) => {
+            this.user = user;
+            this.miFormulario.patchValue({  // Rellenar el formulario con los datos del usuario
+              first_name: this.user.first_name,
+              last_name: this.user.last_name,
+              email: this.user.email,
+              image: this.user.image
+            });
 
-
-  recogerDatosForm() { //Ya NO hay que crear el username y el id para cumplir con el interfaz porque van con _id?: username?:
-    let user = this.miFormulario.value
-    this.user = user;
-    console.log(user)
-    this.usersService.createNewUser(user).subscribe((data: any) => {
-      console.log(data);
-      alert(`Usuario ${user.first_name} ${user.last_name} creado correctamente`)
-      this.miFormulario.reset()
-      // Aquí puedes hacer algo como redirigir a la página de inicio o recargar la lista de usuarios
-    }, (error: any) => {
-      console.log(error);
+          },
+          (error: any) => {
+            console.error(error);
+          }
+        );
+      }
     });
-  }//FALTA AÑADIR EL 
+  }
+
+  recogerDatosForm() {
+    let user = this.miFormulario.value;
+    console.log(user);
+    if (this.actualizando) {
+      // Actualizar usuario existente
+      this.usersService.updateUser(this.user._id).subscribe(
+        (data: any) => {
+          console.log(data);
+          alert(`Usuario ${user.first_name} ${user.last_name} actualizado correctamente`);
+          this.miFormulario.reset();
+          // Aquí puedes hacer algo como redirigir a la página de inicio o recargar la lista de usuarios
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+    } else {
+      // Crear usuario nuevo
+      this.usersService.createNewUser(user).subscribe(
+        (data: any) => {
+          console.log(data);
+          alert(`Usuario ${user.first_name} ${user.last_name} creado correctamente`);
+          this.miFormulario.reset();
+          // Aquí puedes hacer algo como redirigir a la página de inicio o recargar la lista de usuarios
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+
+
+
+
+  /*  recogerDatosForm() { //Ya NO hay que crear el username y el id para cumplir con el interfaz porque van con _id?: username?:
+     let user = this.miFormulario.value
+     this.user = user;
+     console.log(user)
+     this.usersService.createNewUser(user).subscribe((data: any) => {
+       console.log(data);
+       alert(`Usuario ${user.first_name} ${user.last_name} creado correctamente`)
+       this.miFormulario.reset()
+       // Aquí puedes hacer algo como redirigir a la página de inicio o recargar la lista de usuarios
+     }, (error: any) => {
+       console.log(error);
+     });
+   } */
 
   checkControl(pControlName: string, pError: string): boolean {
     if (this.miFormulario.get(pControlName)?.hasError(pError) && this.miFormulario.get(pControlName)?.touched) {
