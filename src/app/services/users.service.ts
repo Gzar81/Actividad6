0ec8b0943177b8929b4ce795eaec9814b9a28e86
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-03-03 13:15:19 +0100

    manejamos errores en el servicio

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { User } from '../interfaces/user.interface';
 
 @Injectable({
@@ -13,22 +13,48 @@ export class UsersService {
   constructor(private httpClient: HttpClient) { }
 
   getAllUsers(): Observable<any> {
-    return this.httpClient.get(`${this.urlBase}`);
+    return this.httpClient.get(`${this.urlBase}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   getById(userId: string): Observable<User> {
-    return this.httpClient.get<User>(`${this.urlBase}/${userId}`);
+    return this.httpClient.get<User>(`${this.urlBase}/${userId}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   createNewUser(user: User): Observable<any> {
-    return this.httpClient.post(`${this.urlBase}`, user);
+    return this.httpClient.post(`${this.urlBase}`, user).pipe(
+      catchError(this.handleError)
+    );
   }
 
   updateUser(userId: string, user: User): Observable<any> {
-    return this.httpClient.put(`${this.urlBase}/${userId}`, user);
+    return this.httpClient.put(`${this.urlBase}/${userId}`, user).pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteUser(userId: string): Observable<any> {
-    return this.httpClient.delete(`${this.urlBase}/${userId}`);
+    return this.httpClient.delete(`${this.urlBase}/${userId}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Se produjo un error en el cliente o en la red.
+      console.error('Ocurrió un error:', error.error.message);
+    } else {
+      // El servidor devolvió una respuesta con un código de error.
+      console.error(
+        `El servidor devolvió un código de error ${error.status}, ` +
+        `el cuerpo del error es: ${error.error}`);
+
+    }
+    return throwError(() => {
+      return 'Ha ocurrido un error. Por favor, inténtelo de nuevo más tarde.'
+    });
   }
 }
